1303c372404894adef496cce6e14800623bb82e9
Carl Brand <11300674+brandc87@users.noreply.github.com>
2024-08-13 15:46:41 +0100

    handle launcher login

--- lib/obsidian/auth.ex
+++ lib/obsidian/auth.ex
@@ -8,6 +8,8 @@ defmodule Obsidian.Auth do
   @cmsg_auth_login 1280
   @cmsg_auth_create_account 1282
 
+  @smsg_auth_login_success 1280
+  @smsg_auth_login_fail 1281
   @smsg_auth_create_account_success 1282
   @smsg_auth_create_account_error 1283
 
@@ -26,20 +28,62 @@ defmodule Obsidian.Auth do
   @impl ThousandIsland.Handler
   def handle_data(
         <<@cmsg_auth_login::little-unsigned-16, _length::little-unsigned-16, packet::binary>>,
-        _socket,
+        socket,
         state
       ) do
     array =
       packet
       |> to_string()
-      |> String.split("/")
+      |> String.split("/", parts: 2)
 
     if length(array) == 2 do
       [username, password] = array
-      Logger.info("CLIENT LOGIN: #{inspect(username)} - #{inspect(password)}")
-    end
 
-    {:continue, state}
+      if account = Accounts.get_account_by_username_and_password(username, password) do
+        Logger.info("AUTH LOGIN: #{account.username} logged in.")
+
+        # TODO: Change this from hard coded.
+        server_list =
+          [
+            %{
+              public_address_ip: "127.0.0.1",
+              public_address_port: 8000,
+              server_name: "Server 1"
+            },
+            %{
+              public_address_ip: "192.168.10.20",
+              public_address_port: 8000,
+              server_name: "Server 2"
+            }
+          ]
+          |> Enum.map(fn server ->
+            "#{server.public_address_ip}:#{server.public_address_port}/#{server.server_name}"
+          end)
+          |> Enum.join("\n")
+
+        reply =
+          <<@smsg_auth_login_success::little-unsigned-16,
+            4 + byte_size(server_list)::little-unsigned-16>> <>
+            server_list
+
+        ThousandIsland.Socket.send(socket, reply)
+
+        {:continue, state}
+      else
+        Logger.info("AUTH LOGIN: #{username} failed logged in.")
+
+        message = "Wrong username or password"
+
+        reply =
+          <<@smsg_auth_login_fail::little-unsigned-16,
+            4 + byte_size(message)::little-unsigned-16>> <>
+            message
+
+        ThousandIsland.Socket.send(socket, reply)
+
+        {:continue, state}
+      end
+    end
   end
 
   @impl ThousandIsland.Handler
